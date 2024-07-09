9dde48b0182ab0fa5e57527be0a1b8216464a02e
Samuel Gillespie <samuel.gillespie@sydney.edu.au>
2024-07-09 12:24:01 +1000

    Added project config and an ignore rule for credentials

--- src/page_consortium.py
+++ src/page_consortium.py
@@ -1,1 +1,9 @@
+#! /usr/bin/env python
+
+#
+# Page Consortium sorts a collection of pages using multiple llm agents.
+#
+# This script orchestrates the flow of information between agents and tracks the consortium's progress towards a complete answer.
+#
+
 print("Hi")
